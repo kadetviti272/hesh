492618a3a5ed0b13759c985cad9a6c7263e1f34d
Blackbearn <Blackbearn777@gmail.com>
2019-02-28 21:31:45 +0200

    getman3

--- src/Slide.java
+++ src/Slide.java
@@ -3,25 +3,45 @@ import java.util.List;
 
 public class Slide {
     List<Photo> slide;
+    List<String> tags_of_slide;
     public Slide(){
         slide = new ArrayList<>();
+        tags_of_slide = new ArrayList<>();
     }
-     public Slide placer(List<Photo> list){
+
+    public List<Photo> getSlide() {
+        return slide;
+    }
+
+    public List<String> getTags_of_slide() {
+        return tags_of_slide;
+    }
+
+    public void setTags_of_slide(List<String> tags_of_slide) {
+        this.tags_of_slide = tags_of_slide;
+    }
+
+    public Slide placer(List<Photo> list){
+        Slide slides = new Slide();
          int count =0;
          for(Photo a : list){
              if(a.getAligment().equals("H")){
-                 slide.add(a);
+                 slides.getSlide().add(a);
+                 slides.setTags_of_slide(a.tegs);
+                 return slides;
              }
              else{
-                 slide.add(a);
+                 slides.getSlide().add(a);
                  for(int i = count; i<list.size(); i++){
                      if(list.get(i).getAligment().equals("V") && a.tegs.contains(list.get(i).tegs)){
-                         slide.add(list.get(i));
+                         slides.getSlide().add(list.get(i));
                      }
                  }
-
+                 return slides;
              }
              count++;
          }
+
+
      }
 }
